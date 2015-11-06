0f7164f46e7e6fcd708eddafa94dd8bfb4b8ec7b
Amitkhanduri <khanduri93@gmail.com>
2015-11-06 12:34:12 +0530

    CORS Enable and Login

--- app/event/eventModule.js
+++ app/event/eventModule.js
@@ -14,8 +14,49 @@ angular.module('eventModule', [])
 	console.log("Event Module::running");
 }])
 .controller('EventCtrl', [function () {
-	var baseUrl = "http://localhost:8111";
+
 	
+	
+}])
+
+.controller('LoginCtrl', ['$scope', '$http', function ($scope, $http) {
+
+	var baseUrl = "http://ec2-52-27-107-78.us-west-2.compute.amazonaws.com:8080"
+
+	console.log("scope is: " + $scope)
+	$scope.submitLogin = function() {
+
+		console.log('in submitLogin');
+
+  		var formdata = "username=" + $scope.user.username + "&password=" + $scope.user.password + "&grant_type=password&scope=read+write&client_secret=123456&client_id=clientapp";
+  		
+  		 $http({
+            method  : 'POST',
+            url     : baseUrl + '/oauth/token',
+            data    : formdata, //forms user object
+            headers : {'Content-Type': 'application/x-www-form-urlencoded',
+                          'Authorization' : "Basic Y2xpZW50YXBwOjEyMzQ1Ng==",
+                          'Accept' : 'application/json'
+                          }
+           })
+            .success(function(response) {
+            	console.log('response' + response);
+              if (response.errors) {
+              console.log("response error: " + $response.errors.name)
+                // Showing errors.
+                $scope.errorName = response.errors.name;                
+                $scope.errorUserName = response.errors.username;
+                $scope.errorEmail = response.errors.email;
+              } else {
+                $scope.access_token = response.access_token;
+                console.log("response: " + $scope.access_token)
+              }
+            })
+            .error(function (data, status, headers, config) {
+                // TODO
+                console.log("response error: " + $response.errors.name)
+            });
+          };
 	
 }])
 
